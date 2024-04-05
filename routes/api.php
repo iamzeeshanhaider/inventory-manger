cec0cca471bbdee97ad444e1fcded9706e551b56
Zeeshan Haider <zeeshansupari001@gmail.com>
2024-04-05 14:13:49 +0500

    routes update

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,13 @@ use Illuminate\Support\Facades\Route;
 
 
 
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 
 ], function ($router) {
-    Route::post('/signup', [AuthController::class, 'signup']);
-    Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::post('/me', [AuthController::class, 'me']);
